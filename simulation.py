7c36e047f1392282ff1c95097b488532e577c689
Denis Kuso <kuso.cm@gmail.com>
2021-09-07 17:45:27 +0100

    Fixed typos

--- simulation.py
+++ simulation.py
@@ -2,14 +2,7 @@ import matplotlib.pyplot as plt
 import visualize as vs
 from room import *
 from robot import *
-# # Run simulations for MIT's room sizes and fraction of room cleaned
-# n_of_ticks = []
-# for room, fraction in zip(room_size,fractions):
-#     # calculate mean time for different room size and different fraction (added as tuple)
-#     n_of_ticks.append((runSimulation(num_robots,speed,room,room,fraction/100,30, StandardRobot),(room,fraction)))
-# for time in range(len(n_of_ticks)):
-#     print(f'Room size {n_of_ticks[time][1][0]} * {n_of_ticks[time][1][0]} takes {round(n_of_ticks[time][0],0)} to clean {n_of_ticks[time][1][1]}%')
-# ==== TESTING FINISHED
+
 
 def runSimulation(num_robots, speed, width, height, min_coverage, num_trials,
                   robot_type, visualize = False):
@@ -57,15 +50,6 @@ def runSimulation(num_robots, speed, width, height, min_coverage, num_trials,
     
     return sum(times)/len(times)
 
-# ==== Testing vs MIT guidelines
-
-# MIT's parameteres
-# room_size =[5,10,10,20]# One dimension of the room, other is the same
-# fractions = [100,75,90,100]# Percentage of room cleaned
-# MIT_n_of_ticks = [150,190,310,3250]# Time taken for room to be cleaned for the desired fraction and size
-# speed = 1.5
-# num_robots = 1
-
 
 def showPlot1(saveFig = False):
     """
@@ -86,14 +70,18 @@ def showPlot1(saveFig = False):
         mean_times.append(runSimulation(robot,SPEED, WIDTH, HEIGHT, CLEAN_FRACTION/100,NUM_TRIALS,StandardRobot))
     
     # Plot
+    fig_size = (10,5)
+    fig = plt.figure(figsize = fig_size)
     title = 'Time to clean fraction of room with varried number of robots'
+    axarr = plt.subplot(1,1,1)
     plt.style.use('ggplot')
+
     plt.plot(robo_number,mean_times,'bo-.',lw=2)
     plt.ylabel(f'Mean time with {NUM_TRIALS} trials')
     plt.minorticks_off()
     plt.xlabel('Number of robots')
     plt.title(f"Time to clean {CLEAN_FRACTION}% of a {WIDTH}x{HEIGHT} room")
-    plt.show()
+    return fig
     
     if saveFig:
         plt.savefig(title + '.png')
@@ -103,63 +91,74 @@ def showPlot2():
     """
     Produces a plot showing dependence of cleaning time on room shape.
     """
-    num_of_robots = 2
-    clean_fraction = 80
-    num_trials = 50
+    NUM_OF_ROBOTS = 2
+    CLEAN_FRACTION = 80
+    NUM_TRIALS = 50
+    SPEED = 1
     
     # Defined room dimensions all of same surface area
-    room_dimension = [(20,20),(25,16),(40,10),(50,8),(80,5),(100,4)]
+    ROOM_DIMENSIONS = [(20,20),(25,16),(40,10),(50,8),(80,5),(100,4)]
     mean_times = []
     wToH_ratio = []
+
+    # Simulate cleaning room with dimensions of room in room_dimension
+    Wdth = 0
+    Hght = 1
+    for room in range(len(ROOM_DIMENSIONS)):
+        wToH_ratio.append(ROOM_DIMENSIONS[room][Wdth]/ROOM_DIMENSIONS[room][Hght])
+        mean_times.append(runSimulation(NUM_OF_ROBOTS,SPEED, ROOM_DIMENSIONS[room][Wdth],ROOM_DIMENSIONS[room][Hght],CLEAN_FRACTION/100,NUM_TRIALS,StandardRobot))
     
-    # Simulate cleaning room with dimensions of room_dimension
-    for room in range(len(room_dimension)):
-        wToH_ratio.append(room_dimension[room][0]/room_dimension[room][1])
-        mean_times.append(runSimulation(num_of_robots,1,room_dimension[room][0],room_dimension[room][1],clean_fraction/100,num_trials,StandardRobot))
-    
-    # Plot 
+    # Plot
+    fig_size = (10,5)
+    fig = plt.figure(figsize = fig_size) 
+    axarr = plt.subplot(1,1,1)
+
     plt.plot(wToH_ratio,mean_times,"bo")
     plt.ylabel('Mean time')
     plt.minorticks_off()
-    plt.xlabel('Width/height')
-    plt.title(f"Time to clean {clean_fraction}% of a room with different width/height ratio")
-    plt.show()
-    plt.close()
-    
+    plt.xlabel('Width/height', size = 6)
+    plt.title(f"Time to clean {CLEAN_FRACTION}% of a room with different width/height ratio")
+    return fig
 
 def showPlot3():
     """
     Produces a plot comparing the two robot strategies.
     """
-    # Instantiate 
+    # Define parameters
     mean_times_stand = []
     mean_times_rand = []
     num_trials = 10
     WIDTH = HEIGHT = 20
-    clean_fraction = 80
+    CLEAN_FRACTION = 80
     NUM_ROBOTS = 15
     SPEED = 1
     robot_number = []
+
     # Simulate cleaning rooms with both types of robots from 1 to NUM_ROBOTS of robots
     for robot in range(1,NUM_ROBOTS + 1):
         robot_number.append(robot)
-        mean_times_stand.append(runSimulation(robot,SPEED, WIDTH, HEIGHT,clean_fraction/100,num_trials,StandardRobot))
-        mean_times_rand.append(runSimulation(robot,SPEED, WIDTH, HEIGHT,clean_fraction/100,num_trials,RandomWalkRobot))
-        
-    
+        mean_times_stand.append(runSimulation(robot,SPEED, WIDTH, HEIGHT,CLEAN_FRACTION/100,num_trials,StandardRobot))
+        mean_times_rand.append(runSimulation(robot,SPEED, WIDTH, HEIGHT,CLEAN_FRACTION/100,num_trials,RandomWalkRobot))
+
     # Plot mean times
+    fig_size = (10,5)
+    fig = plt.figure(figsize = fig_size) 
+    axarr = plt.subplot(1,1,1)
+
     plt.plot(robot_number,mean_times_stand,'bo-.', label = 'Standard Robot' )
     plt.plot(robot_number, mean_times_rand,'ro-.', label = 'Random walk robot')
     plt.legend(loc = 'best')
     plt.ylabel(f'Mean time with {num_trials} trials')
     plt.minorticks_off()
     plt.xlabel('Number of robots')
-    plt.title(f"Time to clean {clean_fraction}% of a {WIDTH}x{HEIGHT} room")
+    plt.title(f"Time to clean {CLEAN_FRACTION}% of a {WIDTH}x{HEIGHT} room")
+    return fig
+
+
+def showPlots():
+    plot1 = showPlot1()
+    plot2 = showPlot2()
+    plot3 = showPlot3()
     plt.show()
 
-# Standard robot for a 20x20 room takes on average at most 1/2 time of Random walk robot
-    # Which seems independet of the number of robots used to clean to room
-
-showPlot1()
-showPlot2()
-showPlot3()
+showPlots()
