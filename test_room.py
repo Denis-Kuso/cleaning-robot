d35d0268cd0be8104f5275e32648f3906b745840
Denis Kuso <kuso.cm@gmail.com>
2021-09-09 15:22:14 +0100

    Test file using unittest

--- test_room.py
+++ test_room.py
@@ -2,10 +2,8 @@ from room import *
 import unittest
 
 class TestRoom(unittest.TestCase):
-    # Create instances of position objects with atributes that test correct output of functions
-    # Same for room objects
+    # Create instances of Position and Room objects 
     
-    # test for invalid input to instances of Room, position, or where input is given?
     def setUp(self):
         print('SetUp')
         self.a = 1
@@ -19,89 +17,62 @@ class TestRoom(unittest.TestCase):
         self.position1 = Position(self.a, self.b)
         self.position2 = Position(self.c, self.d)
 
+    def tearDown(self):
+        pass
+
     def test_getX(self):
-        self.assertEquals(self.position1.getX(),self.a)
+        self.assertEqual(self.position1.getX(),self.a)
     
     def test_getY(self):
-        self.assertEquals(self.position1.getY(),self.b)
+        self.assertEqual(self.position1.getY(),self.b)
+
+    def test_getNewPosition(self):
+        ANGLE = 20
+        SPEED = 10
+        # Old coordinates and change in coordinates
+        old_x = self.position1.getX()
+        old_y = self.position1.getY()
+        delta_x = SPEED * math.sin(math.radians(ANGLE))
+        delta_y = SPEED * math.cos(math.radians(ANGLE))
+        
+        position3 = self.position1.getNewPosition(ANGLE, SPEED)
+        # Expected coordinates
+        exp_x = delta_x + old_x
+        exp_y = delta_y + old_y
+        
+        self.assertIsInstance(position3, Position)
+        self.assertAlmostEqual(position3.getX(),exp_x)
+        self.assertAlmostEqual(position3.getY(),exp_y)
+        
+
     def test_get_width(self):
-        self.assertEquals(self.room1.get_width(),self.w)
+        self.assertEqual(self.room1.get_width(),self.w)
 
     def test_get_height(self):
-        self.assertEquals(self.room1.get_height(),self.h)
+        self.assertEqual(self.room1.get_height(),self.h)
 
     def test_cleanTileAtPosition(self):
-        pass
-        #self.room1.cleanTileAtPosition(self.position1)
-        #self.assertTrue(self.room1.isTileCleaned())
-    
+        # Provide tiles (one clean, one not clean, invalid tile, invalid output)
+        # dirty tile
+        dx = 2
+        dy = 3
+        
+        self.room1.cleanTileAtPosition(self.position1)
+        self.assertTrue(self.room1.isTileCleaned(self.a, self.b))
+        self.assertFalse(self.room1.isTileCleaned(dx, dy))
     
     def test_getNumTiles(self):
-        self.assertEquals(self.room1.getNumTiles(),self.w * self.h)
+        self.assertEqual(self.room1.getNumTiles(), self.w * self.h)
+    
+    def test_getNumCleanedTiles(self):
+        self.assertEqual(self.room1.getNumCleanedTiles(),0)
+
+    def test_getRandomPosition(self):
+        room2 = RectangularRoom(self.w,self.h)
+        new_randPos = room2.getRandomPosition()
+        self.assertIsInstance(new_randPos,Position) 
+        self.assertTrue(room2.isPositionInRoom(new_randPos))
 
     def test_isPositionInRoom(self):
-        pass
-# def test1():
-#     yes = "Passed"
-#     no = 'Failed'
-#     # Instatiate Position object
-#     a = 1
-#     b = 1
-#     position1 = Position(a,b)
-#     # test retrevial of atributes
-#     print('Testing retriveal of atrributes from Position object')
-#     try:
-#         assert position1.getX()== a 
-#         assert position1.getY()== b
-#         print(yes)
-#     except AssertionError:
-#         print(no)
-#     print('\n')
-#     ## Instatiate Rectangular room object
-#     w = 10
-#     h = 10
-#     room1 = RectangularRoom(w,h)
-#     ## test retrievial of atributes
-#     print('Testing retrievial of atrributes from room object')
-#     try:
-#         assert room1.get_width()== w 
-#         assert room1.get_height()== h
-#         print(yes)
-#     except AssertionError:
-#         print(no)
-#     print('\n')
-#     ## Test cleaning of tiles
-#     print('Testing cleaning of tiles')
-#     room1.cleanTileAtPosition(position1)
-#     try:
-#         assert room1.isTileCleaned(a,b) == True
-#         assert room1.isTileCleaned(2,3)== False
-#         print(yes)
-#     except AssertionError:
-#         print(no)
-    
-#     print('\n')
-#     print('Testing number of tiles')
-#     try:
-#         assert room1.getNumTiles() == w*h
-#         position2 = room1.getRandomPosition()
-#         print(yes)
-#     except AssertionError:
-#         print(no)
-#     ## Is position inside the room
-#     print('\n')
-#     print("Testing position in room")
-#     try:
-#         assert room1.isPositionInRoom(position1) == True
-#         print(yes)
-#     except AssertionError:
-#         print(no)
-#     ## Create position outside room
-#     position3 = Position(w + 2, h + 1)
-#     try:
-#         # position 3 should return False, since it is outside the room
-#         assert room1.isPositionInRoom(position3) == False
-#         print(yes)
-#     except AssertionError:
-#         print(no)
-#test1()
+        self.assertTrue(self.room1.isPositionInRoom(self.position1))
+        self.assertFalse(self.room1.isPositionInRoom(self.position2))
